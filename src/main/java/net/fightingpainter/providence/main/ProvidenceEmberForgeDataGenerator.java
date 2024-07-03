46acc86caf6e4a2127c218d0d41f857411137107
Fightingpa1n <fightingpainter@gmail.com>
2024-07-03 03:01:05 +0200

    init

---
+++ src/main/java/net/fightingpainter/providence/main/ProvidenceEmberForgeDataGenerator.java
@@ -0,0 +1,11 @@
+package net.fightingpainter.providence.main;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ProvidenceEmberForgeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
